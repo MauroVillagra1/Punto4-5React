1481b67975816b50f1b121bc366b875d15d924b7
Mauro Villagra <maurovillagra257@gmail.com>
2023-07-23 19:40:24 -0300

    Subir los Puntos 4-5

---
+++ src/componentes/ItemTarea.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { ListGroup, Button } from "react-bootstrap";
+
+const ItemTarea = ({ tarea, onDelete, index }) => {
+  console.log("tarea:", tarea);
+
+  const handleBorrar = () => {
+    onDelete(index);
+  };
+
+  return (
+    <ListGroup.Item className="d-flex justify-content-between">
+     
+  <div>
+    <p>Nombre: {tarea}</p>
+  </div>
+      <Button variant="danger" onClick={handleBorrar}>
+        Borrar
+      </Button>
+    </ListGroup.Item>
+  );
+};
+
+export default ItemTarea;
